a263eec6e26883fed5b2a29abe422814957e3f3d
Jesus Silveira <silveirajesus69@gmail.com>
2024-05-08 18:12:05 -0400

    create pseudocode and logic

---
+++ game.js
@@ -0,0 +1,56 @@
+//create variable for user guess
+let userGuess;
+
+//create variable for machine guess
+let machineGuess;
+
+//create variable for current game number
+let currentGame;
+
+//create variable for user wins counter
+let userWins;
+
+//create variable for machine wins counter
+let machineWins;
+
+//create variable for ties counter
+let ties;
+
+//create variable to keep playing with a boolean value of true
+let keepPlaying = true;
+
+//create function that asks for user input guess and returns it
+function userInputGuess(){
+    userGuess = prompt("Eli")
+}
+
+//create function that returns machine guess
+
+//create while loop that will keep running keep playing variable
+// === n
+
+//if user guess equals machine guess then output "it's a tie!"
+//ties variable += 1
+
+//if user guess equals rock and machine guess equals scissors
+//then output "User wins!"
+
+//if user guess equals scissors and machine guess equals paper
+//then output "User wins!"
+
+//if user guess equals paper and machine guess equals rock
+//then output "User wins!"
+
+//else then output "The machine wins :("
+
+//output "User wins: userWins"
+
+//output "Machine wins: machineWins"
+
+//output "Number of ties: ties"
+
+//ask the user if he wants to play again or not, he must mark
+//"y" or "n" to indicate so
+
+//if input equals n, keepPlaying variable will equal to false
+
